cee1e7eb1a973f6179239b801ce3b1578a881a80
freerafiki <bagigi@disroot.org>
2021-09-10 16:22:16 +0200

    with build

---
+++ build/assets/js/031793e1.1c572dd0.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkprojects=self.webpackChunkprojects||[]).push([[633],{2935:function(a){a.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"/blog/tags/facebook","name":"facebook","count":1,"permalink":"/blog/tags/facebook"}')}}]);
---
+++ build/assets/js/30a24c52.60eb1a3f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkprojects=self.webpackChunkprojects||[]).push([[453],{8605:function(l){l.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"/blog/tags/hello","name":"hello","count":2,"permalink":"/blog/tags/hello"}')}}]);
